6683e35587c77ad1544c9ecdcd1a7f7dcd4a7b94
h4desune <66274785+h4desune@users.noreply.github.com>
2021-06-02 21:22:47 +0300

    Update currentWeatherById.go

--- currentWeatherById.go
+++ currentWeatherById.go
@@ -1,14 +1,13 @@
 package forecastle
 
 import (
-"encoding/json"
-"fmt"
-"io/ioutil"
-"log"
-"net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func CurrentWeatherById(cityId int, unitsOfMeasurement string, outputType string, appid string, lang string) {
+func CurrentWeatherById(cityId int, unitsOfMeasurement string, appid string, lang string) (*CurrentWeatherJson, error) {
 	var url = fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?id=%v&appid=%s&units=%s&lang=%s",
 		cityId,
 		appid,
@@ -18,26 +17,19 @@ func CurrentWeatherById(cityId int, unitsOfMeasurement string, outputType string
 
 	response, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	// TODO: handle all (http) errors that may appear
 
 	body, _ := ioutil.ReadAll(response.Body)
 
-	var jsonHandler currentWeatherJson
+	var jsonHandler CurrentWeatherJson
 
 	err = json.Unmarshal(body, &jsonHandler)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	var output = jsonHandler.Weather[0].Description
-
-	switch {
-	case outputType == "ShowTemperature":
-		fmt.Printf("%v°", jsonHandler.Main.Temp)
-	case outputType == "ShowTemperatureAndDescription":
-		fmt.Printf("%v° - %v", jsonHandler.Main.Temp, output)
-	}
+	return &jsonHandler, nil
 
 }
