17cbf476766cfd649057dca69bb266d24508f155
h4desune <66274785+h4desune@users.noreply.github.com>
2021-06-03 22:23:55 +0300

    Update currentWeatherJson.go

--- currentWeatherJson.go
+++ currentWeatherJson.go
@@ -42,5 +42,5 @@ type CurrentWeatherJson struct {
 	Timezone int    `json:"timezone"`
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
-	Cod      int    `json:"cod"`
+	Cod      string    `json:"cod"`
 }
