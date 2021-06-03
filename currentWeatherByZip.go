d69365c100e0b3b6c2bf1467dcfd9438290d7be0
h4desune <66274785+h4desune@users.noreply.github.com>
2021-06-03 13:01:43 +0300

    Create currentWeatherByZip.go

---
+++ currentWeatherByZip.go
@@ -0,0 +1,43 @@
+package forecastle
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+
+func CurrentWeatherByZip(
+	zip int,
+	countryCode string,
+	appid string,
+	units string,
+	lang string,
+) (*CurrentWeatherJson, error) {
+	var url = fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?zip=%v,%s&appid=%s&units=%s&lang=%s",
+		zip,
+		countryCode,
+		appid,
+		units,
+		lang,
+	)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: handle all (http) errors that may appear
+
+	body, _ := ioutil.ReadAll(response.Body)
+
+	var jsonHandler CurrentWeatherJson
+
+	err = json.Unmarshal(body, &jsonHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	return &jsonHandler, nil
+}
