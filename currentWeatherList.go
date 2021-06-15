c8600f0d3cf1ec705cc1006166dddd8880bfd8a3
h4desune <66274785+h4desune@users.noreply.github.com>
2021-06-15 23:32:46 +0300

    Update currentWeatherList.go

--- currentWeatherList.go
+++ currentWeatherList.go
@@ -2,7 +2,7 @@ package forecastle
 
 type CurrentWeatherList struct {
     Message string               `json:"message"`
-    Cod     string               `json:"cod"`
+    Cod     int               `json:"cod"`
     Count   int              `json:"count"`
     List    []CurrentWeather `json:"list"`
 }
